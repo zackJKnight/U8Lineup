5b2595eb14fbaf8a70b47c1d0eda41babe3dde9f
Zachary Knight <zachary.j.knight@gmail.com>
2018-10-14 10:42:41 -0500

    In progress: Putting Players in positions.

--- u8LineupEngine.ps1
+++ u8LineupEngine.ps1
@@ -20,6 +20,7 @@ Param(
     $TotalPeriods = 4,
     $PeriodDurationMinutes = 12,
     $TotalPositions = 7,
+    $TotalPositionsRanked = 4,
     $RefereeName = 'TestRef',
     $dataFilePath = './u8Lineup.data.json'
 )
@@ -35,63 +36,8 @@ Function Get-GameData {
     $gameData
 }
 
-Function Set-StartingPlayer {
-    Param(
-        [Game]$CurrentGame,
-        [System.Collections.Generic.List[Player]]$AvailablePlayers
-    )
-
-    #get the first period that isn't completely filled. that was my first thought. BUT. Manually, I look at all the periods before I can complete the first period. 
-    #and I often try scenarios with all periods full and then (if filling lineup to win- which is secondary to player pref/skill needs) change names based on ability/endurance
-    #look at number of filled position.
-    #if none, find the goalie first. 
-
-    #need to know open positions.
-    #need to know filled positions and who fills them.
-    $CurrentGame.Periods | ForEach-Object {
-    $UnfilledPositions =  
-        $_ | Select-Object -ExpandProperty Positions | Where-Object {
-            $null -eq $_.StartingPlayer
-        }
-    
-
-    #TODO - build an abstraction to allow user to defined positions.
-    $playersWhoPreferGoalie = $AvailablePlayers | Where-Object {$_.PostionPrefRank -match 'goalie=1'}
-    $playersWhoPreferDefense = $AvailablePlayers | Where-Object {$_.PostionPrefRank -match 'defense=1'}
-    $playersWhoPreferMid = $AvailablePlayers | Where-Object {$_.PostionPrefRank -match 'mid=1'}
-    $playersWhoPreferForward = $AvailablePlayers | Where-Object {$_.PostionPrefRank -match 'forward=1'}
-
-    # can you give a player their first pick?
-    $UnfilledGoalie = $UnfilledPositions | Where-Object { $_.Name -eq 'Goalie' }
-
-    $UnfilledGoalie
-    # can you give a player their second pick?
-    # so on
-    # so forth
-    # When positions are full, fill the bench.
-    # Check that a player hasn't been on the bench yet
-
-    }
-
-    [Player]$bestFitPlayer #= $playersWhoPreferGoalie | Select-Object -First 1
-
-    $bestFitPlayer
-
-}
-
-$GameData = Get-GameData
-
-[Game]$game = [Game]::new((Get-Date))
-
-$game.Ref = $RefereeName
-
-[System.Collections.Generic.List[Player]]$players = New-Object System.Collections.Generic.List[Player]
-
-$GameData.players | ForEach-Object {
-    $players.Add([Player]::new($_.firstName, $_.lastName, $_.playerPositionPreference))
-}
-
-[System.Collections.Generic.List[Position]]$positions = New-Object System.Collections.Generic.List[Position]
+Function New-PositionList {
+    [System.Collections.Generic.List[Position]]$positions = New-Object System.Collections.Generic.List[Position]
 
     #Room for improvement. This is the most basic layout. We can introduce configurable positions later, but I want to work out how to fill a given set of positions.
     $position = [Position]::new()
@@ -116,23 +62,101 @@ $GameData.players | ForEach-Object {
     $position.Name = 'Forward' 
     $positions.Add($position)
 
+    $positions
+}
+
+Function Set-StartingPlayer {
+    [OutputType([Player])]
+    Param(
+        [Game]$CurrentGame,
+        [System.Collections.Generic.List[Player]]$AvailablePlayers,
+        [Position]$CurrentPosition
+    )
+
+    $CurrentPeriod = $CurrentGame.Periods | Where-Object {
+        $CurrentPosition.Id -in ($_.Positions | Select-Object -ExpandProperty Id)
+    }
+    
+    # Let's see what this looks like period by period. then make it smarter.
+    # Manually, I look at all the periods before I can complete the first period. If I don't know much about the players, I can draft the first period without much thought
+    # but the more I know, the more options I have. And as I learn more about the players, the decisions become easier. OH! when the decisions become
+    # too easy, it's time to shake it up to challenge the players. 
+    # I often try scenarios with all periods full and then (if filling lineup to win- which is secondary to player pref/skill needs) change names based on ability/endurance
+    
+    #need to know open positions- or at some point we will. perhaps not right now.
+    # To loop the periods or not to loop the periods... Keep track of a player's starting positions on the player?
+    # as more positions are filled, we'll need to know filled positions and who fills them.
+        
+    [Player[]]$playersWhoPreferCurrentPosition
+    # find preferred position and work our way to least preferred.
+    # if no players prefer this position but the position is empty, pick a random player, until we can 
+    # look at history of who got their top picks and who didn't. 
+    # then we can pick based on who last got what they wanted.
+    
+    # can you give a player their first pick?
+    # can you give a player their second pick?
+    # so on
+    # so forth
+    $i = 1
+
+    DO {    
+        $playersWhoPreferCurrentPosition = $AvailablePlayers | Where-Object {$_.PostionPrefRank -match "$($CurrentPosition.Name)=$($i)"}
+        $i++
+    } Until(($playersWhoPreferCurrentPosition -eq $true -or $playersWhoPreferCurrentPosition.Length -gt 0) -or $i -gt $TotalPositionsRanked)
+
+    # if player is not already starting this period, we can pick one randomly
+    # introduce best fit as this tool gains wisdom
+    [Player[]]$StartingThisPeriod = $CurrentPeriod.Positions | Select-Object -ExpandProperty StartingPlayer
+
+    [Player]$GoodFitPlayer = $playersWhoPreferCurrentPosition | Where-Object {
+        $_ -notin $StartingThisPeriod
+    } | Get-Random 
+
+    if ($null -ne $GoodFitPlayer) {
+        Write-Output $GoodFitPlayer
+    }
+    else {
+        throw "No Player Found to Fill Position: $($CurrentPosition.Name) in Period: $($CurrentPeriod.Number)"
+    }
+}
+
+$GameData = Get-GameData
+
+[Game]$game = [Game]::new((Get-Date))
+
+$game.Ref = $RefereeName
+
+[System.Collections.Generic.List[Player]]$players = New-Object System.Collections.Generic.List[Player]
+
+$GameData.players | ForEach-Object {
+    $players.Add([Player]::new($_.firstName, $_.lastName, $_.playerPositionPreference))
+}
+
 1..$TotalPeriods | ForEach-Object {
     $game.Periods.Add([Period]::new($_, $PeriodDurationMinutes))
 }
 
 $game.Periods | ForEach-Object {
-    $_.Positions = $positions
+    $_.Positions = New-PositionList
 }
 
-$game.Periods | ForEach-Object{
+$game.Periods | ForEach-Object {
     $PeriodPositions = $_ | Select-Object -ExpandProperty Positions 
 
     $PeriodPositions | ForEach-Object {
-        #TODO this is not what I want. because sometimes the starting player will be set, but I'll find a reason to change it as the positions fill in.
-        if($null -eq $_.StartingPlayer){
-    $_.StartingPlayer = (Set-StartingPlayer -CurrentGame $game -AvailablePlayers $players)
+        #TODO this is not what I want. Sometimes the starting player will be set, but I'll find a reason to change it as the positions fill in.
+            
+        if (($null -eq $_.StartingPlayer) -and $($_.Name) -ne 'Bench') {
+            # TODO Learn why your pipeline has an object array with an empty first index
+            $StartingPlayer = Set-StartingPlayer -CurrentGame $game -AvailablePlayers $players -CurrentPosition $_ | Where-Object {$null -ne $_}
+            if($StartingPlayer){
+            $_.StartingPlayer = $StartingPlayer 
+            }
         }
-}
+
+        # When positions are full, fill the bench.
+        # Check that a player hasn't been on the bench yet
+    }
 }
 
 $game
